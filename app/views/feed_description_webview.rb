3d4a8f72f13a230d98488594cd1131cca447f2d8
Ashish Upadhyay <ashish.upadhyaye@gmail.com>
2013-03-20 04:27:40 +0530

    adding feed's description on the webview and

--- app/views/feed_description_webview.rb
+++ app/views/feed_description_webview.rb
@@ -1,10 +1,11 @@
 class FeedDescription < UIViewController
 
+  attr_accessor :feed
 
   def viewDidLoad
-    self.view.backgroundColor = 'blue'.to_color
-    webview = UIWebView.alloc.initWithFrame([[0,40],[320,200]])
-    myHTML = "<html><body><h1>Hello, world!</h1></body></html>";
+    self.title = feed.title
+    webview = UIWebView.alloc.initWithFrame([[0,0],[320,480]])
+    myHTML = feed.description
     webview.loadHTMLString(myHTML, baseURL:nil)
     self.view.addSubview(webview)
   end
--- app/views/main_view.rb
+++ app/views/main_view.rb
@@ -5,6 +5,7 @@ class MainView < UITableViewController
   CELL_REUSE_ID = 'cell_reuse_identifier'
 
   def viewDidLoad
+    self.title = "Events"
     self.feeds = Feed.all
     self.tableView.reloadData
   end
@@ -17,12 +18,14 @@ class MainView < UITableViewController
   def tableView(tableView, cellForRowAtIndexPath:indexPath)
     cell = tableView.dequeueReusableCellWithIdentifier(CELL_REUSE_ID) || UITableViewCell.alloc.initWithStyle(UITableViewCellStyleSubtitle, reuseIdentifier: CELL_REUSE_ID)
     cell.textLabel.text = self.feeds[indexPath.row].title
-    cell.detailTextLabel.text = self.feeds[indexPath.row].description
+    cell.detailTextLabel.text = "#{self.feeds[indexPath.row].description.gsub('</div>','').split('<div>')[1]}"
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator
     cell
   end
 
   def tableView(tableView, didSelectRowAtIndexPath:indexPath)
     description_view = FeedDescription.new
+    description_view.feed = self.feeds[indexPath.row]
     self.navigationController.pushViewController(description_view, animated:true)
   end
 
